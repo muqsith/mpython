8a226fb532ef8172cceabe30384da2b775c79e62
mui <mui@one.com>
2020-02-23 15:05:11 +0400

    Export domains data

---
+++ src/webshop/extractdata.py
@@ -0,0 +1,15 @@
+import os
+import json
+import subprocess
+
+dirname = os.path.dirname(__file__)
+
+with open(os.path.join(dirname, '../../tmp/out.json'), 'r') as f:
+   for line in f:
+       d = json.loads(line)
+       domainName = d["domain"]
+       os.system('sudo /usr/lib/one.com-webshopbackend-app/server/bin/webshop-data export ' + domainName + ' > /home/mui/domainsdata/' + domainName + '.json')
+       break
+       
+
+f.close()
